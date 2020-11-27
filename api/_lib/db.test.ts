66d87a7587337c8565c625f9a5c0f6ee9ae28837
Sebastian Meier <contact@sebastianmeier.eu>
2020-11-27 22:35:27 +0100

    update tests for new db instance

--- api/_lib/db.test.ts
+++ api/_lib/db.test.ts
@@ -4,8 +4,8 @@ describe("db tests", () => {
     expect(getStreet("Platz der Luftbrücke")).toMatchInlineSnapshot(`
       Array [
         Object {
-          "id": 4454,
-          "plz": 0,
+          "id": 5082,
+          "plz": 12101,
           "street": "Platz der Luftbrücke",
         },
       ]
@@ -13,73 +13,41 @@ describe("db tests", () => {
   });
 
   test("getStreetNumber request", () => {
-    expect(getStreetNumber("4454")).toMatchInlineSnapshot(`
+    expect(getStreetNumber("5082")).toMatchInlineSnapshot(`
       Array [
         Object {
-          "id": 166607,
-          "num": "5",
+          "id": 162975,
+          "num": 1
         },
         Object {
-          "id": 166608,
-          "num": "6",
+          "id": 166992,
+          "num": 2
         },
         Object {
-          "id": 166609,
-          "num": "7",
+          "id": 162848,
+          "num": 3
         },
         Object {
-          "id": 166610,
-          "num": "8",
+          "id": 170646,
+          "num": 4
         },
         Object {
-          "id": 166898,
-          "num": "14",
+          "id": 171657,
+          "num": 5
         },
         Object {
-          "id": 166903,
-          "num": "18",
-        },
-        Object {
-          "id": 166904,
-          "num": "19",
-        },
-        Object {
-          "id": 166905,
-          "num": "20",
-        },
-        Object {
-          "id": 193081,
-          "num": "3",
-        },
-        Object {
-          "id": 193082,
-          "num": "4",
-        },
-        Object {
-          "id": 193086,
-          "num": "21",
-        },
-        Object {
-          "id": 193087,
-          "num": "22",
-        },
-        Object {
-          "id": 193088,
-          "num": "23",
-        },
-        Object {
-          "id": 193096,
-          "num": "24",
+          "id": 171923,
+          "num": 6
         },
       ]
     `);
   });
 
   test("getGeoLocationOfHouseNumber request", () => {
-    expect(getGeoLocationOfHouseNumber("166608")).toMatchInlineSnapshot(`
+    expect(getGeoLocationOfHouseNumber("171923")).toMatchInlineSnapshot(`
       Object {
-        "lat": 52.50725,
-        "lon": 13.35146,
+        "lat": 52.48299,
+        "lon": 13.38691,
       }
     `);
   });
