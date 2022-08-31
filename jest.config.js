20c819d1bc5c0ac6195cba5df35084f3a0aa9808
ff6347 <hash@ff6347.email>
2022-08-31 12:36:48 +0200

    test(coverage): Reduce global coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,14 @@ defaultConfig.collectCoverageFrom = ["api/**/*.{ts,tsx}", "api/*.{ts,tsx}"];
 // defaultConfig.globalSetup = "./test/jest.global-setup.js";
 // defaultConfig.globalTeardown = "./test/jest.global-teardown.js";
 defaultConfig.coveragePathIgnorePatterns = [
-  "/node_modules/",
-  "dist",
-  "deprecated",
+	"/node_modules/",
+	"dist",
+	"deprecated",
 ];
+defaultConfig.coverageThreshold.global = {
+	branches: 69,
+	functions: 69,
+	lines: 69,
+	statements: 69,
+};
 module.exports = defaultConfig;
